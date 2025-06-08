bb615d1fcd2d39510a3d0c820609737828008973
ozzkan-burak <brk.ozzkan@gmail.com>
2025-06-08 23:48:46 +0300

    episode1

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Middleware
+//app.use(cors());
+//app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
